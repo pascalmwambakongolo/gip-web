7a20807c0bf2f901c654fd7f06b78cae2851c2ff
PascalMwamba-K <31982145+PascalMwamba-K@users.noreply.github.com>
2017-09-16 09:27:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php 
+<?php
+$gip =microtime(true);
 echo 'test'
-?>
+   //affichage du temps ?>
+<div style="position:fixed; bottom:0; background : #5389a2; color: #fff; line-height:30px; height:30px; right:0; padding-right:30px; text-align:right;">
+
+<?php 
+echo 'Page générée en '.round(microtime(true) - $gip, 5). 'secondes';
+?></div>
